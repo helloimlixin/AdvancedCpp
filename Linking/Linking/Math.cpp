16ed918106c294575391d97cbfc502c6ce7668f5
Xin Li <bulixin@bu.edu>
2019-09-17 18:28:11 +0800

    Fix math.cpp by adding an Entry Point (main method).

---
+++ Linking/Linking/Math.cpp
@@ -0,0 +1,26 @@
+//
+//  Math.cpp
+//  Linking
+//
+//  This will result in a Linker error during Building with the main methodÇ.
+//
+//  Created by Xin Li on 9/17/19.
+//  Copyright © 2019 Xin Li. All rights reserved.
+//
+
+#include <iostream>
+
+void Log(const char* message) {
+    std::cout << message << std::endl;
+}
+
+int Multiply(int a, int b) {
+    Log("Multiply");
+    return a * b;
+}
+
+// Here we have the entry point for the linker to work, note that
+// the entry point does not need to be a main function.
+int main() {
+    std::cout << Multiply(5, 8) << std::endl;
+}

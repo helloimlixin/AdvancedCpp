24db5f8e780e7c2e1552b5ed67f9ed08961cadfe
Xin Li <bulixin@bu.edu>
2019-09-17 18:13:40 +0800

    Created Xcode projects

--- Linking/Linking.md
+++
@@ -1,3 +0,0 @@
-# Linking
-
-After compiling the source file, we come to the process called *Linking*. A primary focus of linking is to find where each symbol and function is and link them together so that multiple `C++` files can interact with each other.
---
+++ Pointers/Pointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pointers
+//
+//  Created by Xin Li on 9/17/19.
+//  Copyright © 2019 Xin Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
